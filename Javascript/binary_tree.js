22a7513f13c7013fefc8ef5adbb8574a10f16a62
Kartik saxena <kartiksaxena@kartiks-MacBook-Pro.local>
2024-02-12 23:59:44 -0500

    build: Tree DFS and BFS implementation

---
+++ Javascript/binary_tree.js
@@ -0,0 +1,115 @@
+class Node {
+    constructor(val=null, left=null, right=null){
+        this.val = val? val : null
+        this.left = left? left :null
+        this.right = right? right : null
+    }
+}
+
+
+//     5
+//    / \
+//   7   20
+//  / \    \
+// 16  3    4
+//    /
+//   29
+
+// DFS (Depth-First Search): 5 7 16 3 29 20 4
+// Time complexity: O(n) || Space complexity: O(n)
+
+// BFS (Breadth-first search): 5 7 20 16 3 4 29
+// Time complexity: O(n) || Space complexity: O(n)
+
+// The above complexity is based on the assumption of using an efficient Queue and Stack capable of adding and removing elememts in O(1).
+
+
+// Declaring Nodes
+
+var head = new Node(5)
+// console.log("Head -> value", head.val)
+// console.log("Head -> left", head.left)
+// console.log("Head -> right", head.right)
+
+var a = new Node(7)
+var b = new Node(20)
+var c = new Node(16)
+var d = new Node(3)
+var e = new Node(4)
+var f = new Node(29)
+
+
+// Relations
+
+head.left = a
+head.right = b
+a.left = c
+a.right = d
+d.left = f
+b.right = e
+
+
+// DFS Iterative
+
+function TreeDFS_iterative(head){
+    if (head==null){
+        console.log("Exmpty tree.")
+    }
+    var bfs = []
+    var stack =[]
+    stack.push(head)
+
+    while(stack.length > 0){
+        var current_node = stack.pop()
+        bfs.push(current_node.val)
+        // Checking right child
+        if (current_node.right != null) stack.push(current_node.right)
+        if (current_node.left != null) stack.push(current_node.left)
+    }
+    return bfs
+}
+
+// console.log(TreeDFS_iterative(head))
+
+
+// DFS Recursive
+
+function TreeDFS_recursive(head){
+    if (head == null){
+        console.log("Exmpty tree.")
+        return []
+    }
+    var a = TreeDFS_recursive(head.left)
+    var b = TreeDFS_recursive(head.right)
+
+    return [head.val, ...a, ...b]
+}
+
+// console.log(TreeDFS_recursive(head))
+
+
+
+// BFS Iterative
+
+function TreeBFS_iterative(head){
+    if (head==null){
+        console.log("Exmpty tree.")
+    }
+    var queue = [head]
+    var bfs = []
+
+    while(queue.length > 0){
+        var current_node = queue.shift()
+        bfs.push(current_node.val)
+        if (current_node.left != null) {queue.push(current_node.left)}
+        if (current_node.right != null) {queue.push(current_node.right)}
+    }
+    return bfs
+}
+
+// console.log(TreeBFS_iterative(head));
+
+
+
+// BFS Recursive
+// BFS does not have an efficient recursive approach, as BFS uses a Queue to empliment and using Recursion uses Stack under the hood which doesnt really align with the implimentation of BFS.
